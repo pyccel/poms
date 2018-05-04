51a3f9c2ab71f175caca102855d3d0d36ac13aa8
Jalal <jlakhlili@gmail.com>
2018-05-04 23:48:31 +0200

    use update_ghost_regions from spl

--- kron_product.py
+++ kron_product.py
@@ -5,44 +5,8 @@ from spl.ddm.cart       import Cart
 from spl.linalg.stencil import StencilVectorSpace, \
                                StencilVector, StencilMatrix
 
-# ... TODO use the SPL version when available
-def update_ghost_regions(stencil_vector, direction):
-
-    # ...
-    data  = stencil_vector._data
-    space = stencil_vector._space
-    cart  = space.cart
-    comm  = cart.comm_cart
-
-    # ... Choose non-negative invertible function tag(disp) >= 0
-    # ... NOTE: different values of disp must return different tags!
-    tag = lambda disp: 42+disp
-
-    # Requests' handles
-    requests = []
-
-    # ... Start receiving data (MPI_IRECV)
-    for disp in [-1,1]:
-        info     = cart.get_shift_info(direction, disp)
-        recv_typ = space.get_recv_type(direction, disp)
-        recv_buf = (data, 1, recv_typ)
-        recv_req = comm.Irecv(recv_buf, info['rank_source'], tag(disp))
-        requests.append(recv_req)
-
-    # ... Start sending data (MPI_ISEND)
-    for disp in [-1,1]:
-        info     = cart.get_shift_info(direction, disp)
-        send_typ = space.get_send_type(direction, disp)
-        send_buf = (data, 1, send_typ)
-        send_req = comm.Isend(send_buf, info['rank_dest'], tag(disp))
-        requests.append(send_req)
-
-    # ... Wait for end of data exchange (MPI_WAITALL)
-    MPI.Request.Waitall(requests)
-# ...
-
 # .. Fill in stencil matrix as band
-def populate_1d_matrix(M):
+def populate_1d_matrix(M, diag):
     e = M.ends[0]
     s = M.starts[0]
     p = M.pads[0]
@@ -50,11 +14,12 @@ def populate_1d_matrix(M):
     for i in range(s, e+1):
         for k in range(-p, p+1):
             M[i, k] = k
-        M[i, 0] = 5.*p
+        M[i, 0] = diag
     M.remove_spurious_entries()
 # ...
 
-n1 = 16
+# ...
+n1 = 8
 n2 = 8
 p1 = 2
 p2 = 1
@@ -84,8 +49,8 @@ A = StencilMatrix(V2, V2)
 # ..
 
 # ... Populate A, B and X
-populate_1d_matrix(A)
-populate_1d_matrix(B)
+populate_1d_matrix(A, 5.)
+populate_1d_matrix(B, 8.)
 
 
 # ... Exchange Ghosts cell for X
@@ -99,34 +64,38 @@ Y[:,:]    = 0.
 # ... Compute Kron prod
 for i1 in range(V1.npts[0]):
 
-    for i2 in range(V2.npts[0]):
+    i1_glob = i1 + V.starts[0]
 
-        Xtmp[i1, i2] = 0.
+    for i2 in range(V2.npts[0]):
+        i2_glob = i2 + V.starts[1]
+
+        Xtmp[i1_glob, i2_glob] = 0.
 
         for k2 in range(-p2, p2+1):
-            j2 = i2 + k2
-            Xtmp[i1, i2] += X[i1, j2]*A[i2, k2]
+            j2_glob = i2_glob + k2
+            Xtmp[i1_glob, i2_glob] += X[i1_glob, j2_glob]*A[i2, k2]
 
-        update_ghost_regions(Xtmp, direction=1)
-        update_ghost_regions(Xtmp, direction=0)
+        Xtmp.update_ghost_regions(direction=1)
 
         # ...
         for k1 in range(-p1, p1+1):
-            j1 = i1 + k1
-            Y[i1, i2] += B[i1, k1]*Xtmp[j1, i2]
+            j1_glob = i1_glob + k1
+            Y[i1_glob, i2_glob] += B[i1, k1]*Xtmp[j1_glob, i2_glob]
 
-        update_ghost_regions(Y, direction=0)
-        update_ghost_regions(Y, direction=1)
+        Y.update_ghost_regions(direction=0)
         # ...
 # ...
 
 for i in range(comm.Get_size()):
     if rank == i:
-        print('rank= ', rank)
-        print('A=', A.toarray())
-        print('B=', B.toarray())
-        print('X=', X.toarray())
-        print('Y=', Y.toarray())
+#        print('rank= ', rank)
+#        print('A=', A.toarray())
+#        print('B=', B.toarray())
+#        print('X=', X.toarray())
+#        print('Y=', Y.toarray())
+#        print('================')
+        print('n1 = ', n1)
+        print('nv = ', cart.ends[0] - cart.starts[0] + 1)
         print('', flush=True)
     comm.Barrier()
 
