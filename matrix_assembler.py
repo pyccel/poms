b4756a0ae498b2026a100749c15b4f25bbffa7dc
Jalal Lakhlili <jalal.lakhlili@ipp.mpg.de>
2018-05-15 18:30:23 +0200

    update matrix assembler module

--- matrix_assembler.py
+++ matrix_assembler.py
@@ -72,34 +72,3 @@ def assembly(V, kernel):
 
             M[i1:i1+p1+1,i2:i2+p2+1,:,:] += mat[:,:,:,:]
 # ...
-
-#===============================================================================
-if __name__ == "__main__":
-
-    # ... numbers of elements and degres
-    p1  = 2  ; p2  = 2
-    ne1 = 32 ; ne2 = 32
-    # ...
-
-    comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()
-
-    if rank == 0:
-        print('> Grid   :: [{ne1},{ne2}]'.format(ne1=ne1, ne2=ne2))
-        print('> Degree :: [{p1},{p2}]'.format(p1=p1, p2=p2))
-
-    grid_1 = linspace(0., 1., ne1+1)
-    grid_2 = linspace(0., 1., ne2+1)
-
-    V1 = SplineSpace(p1, grid=grid_1)
-    V2 = SplineSpace(p2, grid=grid_2)
-
-    V = TensorFemSpace(V1, V2, comm=comm)
-
-    # ...
-    wt = MPI.Wtime()
-    assembly(V, kernel)
-    wt = MPI.Wtime() - wt
-
-    print('rank: ', rank, '> Elapsed time: {}'.format(wt))
-    # ...
